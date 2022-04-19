ce676865f5f0d0e8ee1a37c30772658a0f48d252
Jordan <63085677+jordantan2@users.noreply.github.com>
2022-04-19 19:33:22 +0800

    Todo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
-import TodoItem from "./components/TodoItem";
-import TodoForm from "./components/TodoForm"
+import TodoItem from "./components/TodoItem/TodoItem";
+import TodoForm from "./components/TodoForm/TodoForm"
 import { useState } from "react";
-
-
+import "./App.css"
 
 
 function App() {
   const [todos, setTodos] = useState([
-    "Learn React",
-    "Tell friends to learn react",
-    "get them to tell 5 friends",
-    "Start successful pyramid scheme"
+    { text: "Learn about React", isCompleted: false},
+    { text: "Meet friends for lunch", isCompleted: false},
+    { text: "Pick Jim up from airport", isCompleted: false},
   ]);
 
 
   const addTodo = (text) => {
-    const newTodos = [...todos, text];
+    const newTodos = [...todos, { text: text, isCompleted: false }];
     setTodos(newTodos);
 
   };
 
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
   return (
-    <div>
-      <h1>My todo list</h1>
-      {todos.map((todo, index) => (
-        <TodoItem todo={todo} key={index} />
-      ))}
-      <TodoForm addTodo={addTodo} />
+    <div className="app">
+      <div className="todo-list">
+        <h1>My todo list</h1>
+        {todos.map((todo, index) => (
+          <TodoItem 
+          todo={todo} 
+          key={index}
+          index={index}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoItem/TodoItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./TodoItem.css";
+
+
+function TodoItem(props) {
+    const { todo, index, completeTodo, removeTodo } = props;
+   
+   return ( 
+     <div className={`todo ${todo.isCompleted ? "complete" :""}`}>
+        {todo.text} 
+        <div>
+            <button onClick={() => completeTodo(index)}>Complete</button>
+            <button onClick={() => removeTodo(index)}>X</button>
+        </div>
+      </div>
+   );
+}
+
+export default TodoItem;
